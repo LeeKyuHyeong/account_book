6dc76a4289e447006f0508478890fd958659e4a4
이규형 <rbgud2380@naver.com>
2023-11-13 19:52:40 +0900

    231113 commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,56 @@ import { BrowserRouter, Routes, Link, Route } from "react-router-dom";
 import './styles/common/App.css';
 import Nav from "./components/Nav";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import List from './pages/List';
 
 function App() {
 
+  const ViewNotice = ({ noticeData }) => {
+    return (
+      <div key={noticeData.idx}>
+        <h2>{noticeData.idx}. {noticeData.title}</h2>
+        <p>{noticeData.content}</p>
+        {/* <tr key={userData}>
+          <td>{userData.name}</td>
+          <td>{userData.email}</td>
+        </tr> */}
+      </div>
+    )
+  }
+
+  let [notice, setNotice] = useState(
+    [
+      {
+        idx: 1,
+        'title': '공지사항 첫번째',
+        'content': '이것저것 지키세요'
+      },
+      {
+        idx: 2,
+        'title': '공지사항 두번째',
+        'content': '이것저것 지키세요 제발'
+      }
+    ]
+  );
+
+  function 내용바꾸기() {
+    let newArr = [...notice];
+    if (newArr[0].content.includes('수정')) {
+      newArr[0].content = '이것저것 돌아오기~';
+    } else {
+      newArr[0].content = '수정된 내용입니다.';
+    }
+    setNotice(newArr);
+  }
 
   return (
     <div className="App">
       <Nav />
-
+      <List />
+      <div className="notice">
+        <button onClick={내용바꾸기}>버튼</button>
+        {notice.map(e => <ViewNotice noticeData={e} />)}
+      </div>
       {/* <Routes>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,6 +1,7 @@
-import React from "react";
+import { React } from "react";
+import "../styles/common/List.css";
 
-const User = ({userData}) => {
+const User = ({ userData }) => {
   return (
     <tr key={userData}>
       <td>{userData.name}</td>
@@ -11,25 +12,29 @@ const User = ({userData}) => {
 
 const List = () => {
   const users = [
-    {email: 'ryu@gmail.com', name:'유재석'},
-    {email: 'kim@gmail.com', name:'김종국'},
-    {email: 'ha@gmail.com', name:'하하'},
-    {email: 'song@gmail.com', name:'송지효'},
-    {email: 'jeon@gmail.com', name:'전소민'}
+    { email: 'ryu@gmail.com', name: '유재석' },
+    { email: 'kim@gmail.com', name: '김종국' },
+    { email: 'ha@gmail.com', name: '하하' },
+    { email: 'song@gmail.com', name: '송지효' },
+    { email: 'jeon@gmail.com', name: '전소민' }
   ];
 
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>이름</th>
-          <th>이메일</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map(user => <User userData={user}/>)}
-      </tbody>
-    </table>
+    <div className="userTbl">
+      <table>
+        <thead>
+          <tr>
+            <th>이름</th>
+            <th>이메일</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => <User userData={user} />)}
+        </tbody>
+        <div className="btn">변경</div>
+      </table>
+    </div>
   )
 }
 
